7f7af3483f9b8f0e16aded59640b8e744cdaadd4
24249270 Andries Bester <24249270>
2017-08-08 16:08:12 +0200

    Andries add Stat + TestNum Classes

---
+++ groep20_git/groep20_git/TestNumber.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace groep20_git
+{
+    class TestNumber
+    {
+        public bool test(int input)
+        {
+            if (input <= 20 && input >= 5)
+                return true;
+            else
+                return false;
+        }
+    }
+}
