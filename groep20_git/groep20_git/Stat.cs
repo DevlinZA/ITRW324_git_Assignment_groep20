b349dd1e717209e6347577c6d36b80a1cddba69f
DESKTOP-GR3CRQ0\Johannnnnn <25847465@student.g.nwu.ac.za>
2017-08-08 16:24:45 +0200

    Moved median to stat

--- groep20_git/groep20_git/Stat.cs
+++ groep20_git/groep20_git/Stat.cs
@@ -8,5 +8,21 @@ namespace groep20_git
 {
     class Stat
     {
+        public double calcMedian(int[] numbers)
+        {
+            int numberCount = numbers.Count();
+            int halfIndex = numbers.Count() / 2;
+            var sortedNumbers = numbers.OrderBy(n => n);
+            double median;
+            if ((numberCount % 2) == 0)
+            {
+                median = ((sortedNumbers.ElementAt(halfIndex) + sortedNumbers.ElementAt((halfIndex - 1))) / 2);
+            }
+            else
+            {
+                median = sortedNumbers.ElementAt(halfIndex);
+            }
+            return median;
+        }
     }
 }
