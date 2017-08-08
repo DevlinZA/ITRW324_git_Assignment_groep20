4bf5560b2134454497198f2bc9bb2e970196209b
DevlinZA <jaco.vanzyl@creative-sound.net>
2017-08-08 16:37:17 +0200

    MIN word getoets

--- groep20_git/groep20_git/frmJaco.cs
+++ groep20_git/groep20_git/frmJaco.cs
@@ -13,6 +13,10 @@ namespace groep20_git
     public partial class frmJaco : Form
     {
         clMIN cmin = new clMIN();
+        TestNumber tstNum = new TestNumber();
+
+        
+        
 
         public frmJaco()
         {
@@ -21,7 +25,14 @@ namespace groep20_git
 
         private void button1_Click(object sender, EventArgs e)
         {
-            cmin.numGen(Convert.ToInt16(txt1.Text));
+           if( tstNum.test(Convert.ToInt16(txt1.Text)))
+            {
+                cmin.numGen(Convert.ToInt16(txt1.Text));
+            }else
+            {
+                MessageBox.Show(" Skies die getal is nie tussen 5 en 20 nie");
+            }
+            
         }
     }
 }
