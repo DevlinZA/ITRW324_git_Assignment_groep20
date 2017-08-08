5f2e937cd93f515106957315bdda6824899eeca3
Ruan du Plessis <ruan@ics.co.za>
2017-08-08 14:52:23 +0200

    Added Class and fixes

--- groep20_git/groep20_git/AddMax.cs
+++ groep20_git/groep20_git/AddMax.cs
@@ -7,25 +7,25 @@ using System.Windows.Forms;
 
 namespace groep20_git
 {
-    //class AddMax
-    //{
-    //    public AddMax(int number)
-    //    {
-    //        int max = 0;
-    //        Random rand = new Random();
-    //        int[] numbers = new int[number];
-    //        for(int i = 0; i < number; i++)
-    //        {
-    //            numbers[i] = rand.Next(101);
-    //        }
-    //        for (int i = 0; i < number; i++)
-    //        {
-    //            if(max <= numbers[i])
-    //            {
-    //                max = numbers[i];
-    //            }
-    //        }
-    //        MessageBox.Show("The max value for the generated numbers is: " + Convert.ToString(max));
-    //    }
-    //}
+    class AddMax
+    {
+        public AddMax(int number)
+        {
+            int max = 0;
+            Random rand = new Random();
+            int[] numbers = new int[number];
+            for (int i = 0; i < number; i++)
+            {
+                numbers[i] = rand.Next(101);
+            }
+            for (int i = 0; i < number; i++)
+            {
+                if (max <= numbers[i])
+                {
+                    max = numbers[i];
+                }
+            }
+            MessageBox.Show("The max value for the generated numbers is: " + Convert.ToString(max));
+        }
+    }
 }
